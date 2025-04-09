594ae61154154bd76906044bb70d3b8455eb7610
mstekiel <64463131+mstekiel@users.noreply.github.com>
2025-04-09 14:17:56 +0200

    Announce internal error policy

--- neutronpy/instrument/exceptions.py
+++ neutronpy/instrument/exceptions.py
@@ -1,46 +1,56 @@
 # -*- coding: utf-8 -*-
-"""Custom Exceptions for the instrument module
+"""Custom Errors for the instrument module.
 
+DEV:
+Every error is a subclass of `Warning`, so that it can be used either as a:
+- warning: warn(SomeProblem('Some message'))
+- exception: raise SomeProblem('Some message')
+
+Idea here was to continue any calculations despite wrong input parameters.
+This seems to be what numpy is doing and I like it.
 """
+from warnings import warn
 
-
-class DetectorError(Exception):
+class DetectorError(Warning):
     """For Exceptions related to the properties of the Detector
     """
     pass
 
 
-class ScatteringTriangleError(Exception):
+class ScatteringTriangleNotClosed(Warning):
     """For Exceptions related to the Scattering Triangle not closing
     """
     pass
 
+class VectorNotInScatteringPlane(Warning):
+    """For Exceptions related to vector not in scattering plane
+    """
+    pass
 
-class MonochromatorError(Exception):
+class MonochromatorError(Warning):
     """For Exceptions related to the Monochromator properties
     """
     pass
 
 
-class AnalyzerError(Exception):
+class AnalyzerError(Warning):
     """For Exceptions related to the Monochromator properties
     """
     pass
 
-
-class ChopperError(Exception):
+class ChopperError(Warning):
     """For Exceptions related to the Chopper properties
     """
     pass
 
 
-class GoniometerError(Exception):
+class GoniometerError(Warning):
     """For Exceptions related to the Goniometer positions
     """
     pass
 
 
-class InstrumentError(Exception):
+class InstrumentError(Warning):
     """For general, unclassified types of Instrument Exceptions
     """
     pass
